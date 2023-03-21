018224d10323932ce9d636c7bab5869da8073380
Mukesh B <85065063+Mukesh-areo@users.noreply.github.com>
2023-03-21 22:20:27 +0530

    setup added

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup,find_packages
+from typing import List
+
+#Declaring variables for setup functions
+PROJECT_NAME="Air-Pressure-System-Sensor-Fault-Preventive-Maintenance"
+VERSION="0.0.1"
+AUTHOR="Mukesh B"
+DESRCIPTION="In this project, the system in focus is the Air Pressure system (APS) which generates pressurized air that are utilized in various functions in a truck, such as braking and gear changes. The datasets positive class corresponds to component failures for a specific component of the APS system. The negative class corresponds to trucks with failures for components not related to the APS system.The problem is to reduce the cost due to unnecessary repairs. So it is required to minimize the false predictions."
+
+REQUIREMENT_FILE_NAME="requirements.txt"
+
+HYPHEN_E_DOT = "-e ."
+
+
+def get_requirements_list() -> List[str]:
+    """
+    Description: This function is going to return list of requirement
+    mention in requirements.txt file
+    return This function is going to return a list which contain name
+    of libraries mentioned in requirements.txt file
+    """
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+        return requirement_list
+
+
+
+setup(
+name=PROJECT_NAME,
+version=VERSION,
+author=AUTHOR,
+description=DESRCIPTION,
+packages=find_packages(), 
+install_requires=get_requirements_list()
+)
